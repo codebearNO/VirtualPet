235a5006122e02480cfaed1bef15f6c2c8712e4f
Codebear <thorbjorn.hoib@hotmail.com>
2023-11-22 13:34:12 +0100

    Init

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    internal class Pet
+    {
+        public string Name { get;  }
+        public int Age { get; set; }
+        public bool Hungry = true;
+        public double Cuddle = 50;
+        public double CuddleRate = 0;
+
+
+        public Pet(string name, int age, double cuddleRate)
+        {
+            Name = name;
+            Age = age;
+            CuddleRate = cuddleRate;
+        }
+        public void Feed()
+        {
+            Console.WriteLine($"You fed {Name}");
+            Hungry = false;
+        }
+
+        public void AgePet()
+        {
+            Age++;
+        }
+
+        public void Toilet()
+        {
+            // Bladder = empty
+            Hungry = true;
+        }
+        
+        public void CuddleBuildUp()
+        {
+            if (Cuddle > 0)
+            {
+                Cuddle -= CuddleRate;
+            }
+            else
+            {
+                gameRunning = false;
+                Console.Clear();
+                Console.WriteLine("Game Over");
+            }
+        }
+
+        public void CuddlePet()
+        {
+            Cuddle += 25;
+        }
+
+
+    }
+}
+
+//class Car
+//{
+//    public string model;
+
+//    // Create a class constructor with a parameter
+//    public Car(string modelName)
+//    {
+//        model = modelName;
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        Car Ford = new Car("Mustang");
+//        Console.WriteLine(Ford.model);
+//    }
+//}
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -2,10 +2,40 @@
 {
     internal class Program
     {
+        //Virtual Pet
+        // Lag en konsoll-applikasjon hvor man kan generere opp flere forskjellige virtuelle kjæledyr.
+        // Bruk constructor i klassen, og man skal ikke ha lov til å endre data som navn, age eller noen andre egenskaper utenfra klassen.
+        // Lag funksjonalitet som gjør at man kan gi dyret mat, kose med dyret eller sjekke om dyret må på do
+        //
+        // Her er et eksempel på hvordan det kan se ut:
+        //
+        // Hvilket dyr vil du ta vare på? 
+        // Pikachu
+        // 1. Gi Pikachu mat
+        // 2. Kos med Pikachu
+        // 3. Sjekk om Pikachu må på do
+        // 2
+        // Pikachu smiler!
+        // 1. Gi Pikachu mat
+        // 2. Kos med Pikachu
+        // 3. Sjekk om Pikachu må på do
+        // 1
+        // Pikachu er mett og fornøyd
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            
+            Console.WriteLine("Hello! Name your pet:");
+            string input = Console.ReadLine();
+            Pet myPet = new Pet(input, 15, 2);
+            bool gameRunning = true;
+            while (gameRunning)
+            {
+                Console.Clear();
+                Console.WriteLine($"Your pet: {myPet.Name}");
+                Console.WriteLine($"Age: {myPet.Age}"); 
+                myPet.CuddleBuildUp();
+                if(myPet.Cuddle < 25)Console.WriteLine(myPet.Cuddle);
+                Thread.Sleep(100);
+            }
         }
     }
 }
